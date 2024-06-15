013a29564645140325339ecdcae6d58b4346702a
Jose Agustin Valdivia Guzman <u201822153@upc.edu.pe>
2024-06-15 11:38:44 -0500

    feat: Implementacion angular 17

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SesionRegistroComponent } from './sesion-registro/sesion-registro.component';
+import { MensajesComponent } from './mensajes/mensajes.component';
+import { LandingComponent } from './landing/landing.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SesionRegistroComponent,
+    MensajesComponent,
+    LandingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/mensajes/mensajes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mensajes',
+  templateUrl: './mensajes.component.html',
+  styleUrl: './mensajes.component.css'
+})
+export class MensajesComponent {
+
+}
