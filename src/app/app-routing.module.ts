1b536629b9db4bab085986ea7b6a7c517dc7ea2c
juanpi879 <145165598+juanpi879@users.noreply.github.com>
2024-06-15 23:23:33 -0500

    app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MensajesComponent } from './mensajes/mensajes.component';
 import { CalificacionPropietarioComponent } from './calificacion-propietario/calificacion-propietario.component';
 import { SesionRegistroComponent } from './sesion-registro/sesion-registro.component';
+import { PublicarComponent } from './publicar/publicar.component';
 
 const routes: Routes = [
   { path: 'mensajes', component: MensajesComponent },
   { path: 'calificacion-propietario', component: CalificacionPropietarioComponent },
   { path: 'sesion-registro', component: SesionRegistroComponent},
+  { path: 'publicar', component: PublicarComponent},
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
   { path: '**', redirectTo: '' }
 ];
