f61a844bb273d01557b6019c5703cfb550e75421
juanpi879 <145165598+juanpi879@users.noreply.github.com>
2024-06-15 23:22:16 -0500

    Add files via upload

---
+++ src/app/publicar/publicar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-publicar',
+  templateUrl: './publicar.component.html',
+  styleUrl: './publicar.component.css'
+})
+export class PublicarComponent {
+
+}
